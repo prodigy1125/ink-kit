156dad78a2ebd22e6390546cf189bb88b94fb412
prodigyray1125 <vokaraon@gmail.com>
2025-02-05 14:28:58 -0500

    feat: update icons

--- src/icons/AllIcons.tsx
+++ src/icons/AllIcons.tsx
@@ -1,7 +1,12 @@
 import { classNames } from "../util/classes";
 import * as Icons from "./index";
+import "./AllIcons.css";
 
-const icons = Object.values(Icons);
+type IconType = (typeof Icons)[keyof typeof Icons];
+
+interface IconsOrFolder {
+  [key: string]: IconType | IconsOrFolder;
+}
 
 export const AllIcons: React.FC<{
   containerClassName?: string;
@@ -10,16 +15,71 @@ export const AllIcons: React.FC<{
   return (
     <div
       className={classNames(
-        "ink:flex ink:flex-wrap ink:gap-2",
+        "ink:flex ink:flex-wrap ink:gap-2 ink:text-text-default",
         containerClassName
       )}
     >
-      {icons.map((Icon) => (
-        <Icon
-          className={classNames("ink:size-4", iconClassName)}
-          key={Icon.name}
-        />
-      ))}
+      <div className="ink:text-body-2-bold">Click to copy icon name</div>
+      <IconsOrFolder
+        title="InkIcon"
+        iconsOrFolder={Icons}
+        iconClassName={classNames("tooltip-on-hover", iconClassName)}
+      />
     </div>
   );
 };
+
+function IconsOrFolder({
+  title,
+  iconsOrFolder,
+  iconClassName,
+}: {
+  title: string;
+  iconsOrFolder: IconsOrFolder;
+  iconClassName?: string;
+}) {
+  return (
+    <div className={classNames("ink:flex ink:flex-col ink:gap-2 ink:pl-2")}>
+      <div className="ink:text-caption-1-bold ink:text-text-muted">{title}</div>
+
+      <div className="ink:flex ink:flex-wrap ink:gap-2">
+        {Object.entries(iconsOrFolder).map(([name, IconOrFolder]) => {
+          if (!isIconFolder(IconOrFolder)) {
+            return (
+              <div
+                className="tooltip-on-hover ink:cursor-pointer ink:whitespace-nowrap"
+                data-title={`<${title}.${name} />`}
+                onClick={() =>
+                  navigator.clipboard.writeText(`<${title}.${name} />`)
+                }
+              >
+                <IconOrFolder
+                  className={classNames("ink:size-4", iconClassName)}
+                  title={`${title}.${name}`}
+                  key={name}
+                />
+              </div>
+            );
+          }
+        })}
+      </div>
+
+      {Object.entries(iconsOrFolder).map(([name, IconOrFolder]) => {
+        if (isIconFolder(IconOrFolder)) {
+          return (
+            <IconsOrFolder
+              key={name}
+              title={`${title}.${name}`}
+              iconsOrFolder={IconOrFolder}
+              iconClassName={iconClassName}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+}
+
+function isIconFolder(icon: IconType | IconsOrFolder): icon is IconsOrFolder {
+  return typeof icon === "object";
+}
